d2157f05ab7b8b5873584c1fca1f1df8689c8357
satya.vishnu <satya.vishnu@CSELAB02-14.bennett.edu.in>
2017-08-21 11:59:22 +0530

    12

---
+++ Sorting/src/NewSort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class NewSort {
+
+	public static void main(String[] args) {
+		int n,c;
+		Scanner in = new Scanner(System.in);
+	    System.out.println("Input number of integers to sort");
+	    n = in.nextInt();
+	    int array[] = new int[n];
+	    System.out.println("Enter " + n + " integers");
+	    for (c = 0; c < n; c++) 
+	      array[c] = in.nextInt();
+	    Arrays.sort(array);
+	     for(int i: array)
+	     {
+	    	 System.out.println(i);
+	     }
+	     }
+		// TODO Auto-generated method stub
+
+	}
+
+
